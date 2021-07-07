b3e5a533e59a1f8d43c18a60982e98c3386eb0c7
Yuki Yamamoto <yukiyamamoto710@gmail.com>
2021-07-07 11:17:54 -0700

    add styling to the Biography screen

--- screens/Biography.js
+++ screens/Biography.js
@@ -1,30 +1,44 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
+import { SafeAreaView, ScrollView, Text, Image, StyleSheet } from 'react-native';
 
 const Biography = (props) => {
   const bio = JSON.parse(props.bio);
-  console.log({url: `${bio._links.thumbnail.href}`});
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Image source={{url: `${bio._links.thumbnail.href}`}} style={styles.image}/>
-      <Text>{bio.name}</Text>
-      <Text>{bio.birthday}-{bio.deathday}</Text>
-      <Text>{bio.nationality}</Text>
-      <Text>{bio.biography}</Text>
+      <Text style={styles.name}>{bio.name}</Text>
+      <Text style={styles.small}>{bio.birthday}-{bio.deathday}</Text>
+      <Text style={styles.small}>{bio.nationality}</Text>
+      <ScrollView style={styles.scroll}>
+        <Text style={styles.bio}>{bio.biography}</Text>
+      </ScrollView>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   image: {
-    position: 'relative',
-    top: 55,
-    right: 0,
-    left: 0,
-    justifyContent: 'center',
-    width: 350,
-    height: 200,
+    flex: 1,
+    resizeMode: 'contain',
+  },
+  name: {
+    fontSize: 24,
+    right: -50,
+  },
+  small: {
+    fontSize: 16,
+    right: -50,
+    paddingVertical: 5,
+  },
+  scroll: {
+    flex: 1,
+    paddingHorizontal: 50,
+    paddingVertical: 5,
+    color: 'black',
   }
 })
 export default Biography;
